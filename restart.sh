a76de8a4655b415dd5bcc24d185119efd972e34e
Ovidiu Chereches <skidding@gmail.com>
2013-05-17 13:12:19 +0300

    Tweak restart script

--- restart.sh
+++ restart.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 # Check if the Sinatra app is still running
-if ps aux | grep "ruby app.rb" | grep -v "grep"
+if ps aux | grep "app.rb" | grep -v "grep"
 then
   # Carry on, don't persist log in order to avoid spam
   echo "$(date) UberNews running successefully"
@@ -10,7 +10,7 @@ else
   # Log this occurange
   echo "$(date) Darm, UberNews crashed again! :(" >> ./crash-log.txt
   # Restart the Sinatra app
-  ruby app.rb &
+  ruby app.rb > ./app-output.txt &
   # Restart Apache because for some reason it no longer forwards correctly to
   # the application port once it restarts---TODO investigate
   /etc/init.d/apache2 restart

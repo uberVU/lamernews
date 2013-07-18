dcc8edf35fcabf95ddb16c0368305c63f433ef85
root <root@ubvu-blog1.ubervu.local>
2013-07-18 15:30:04 +0000

    Solved restart.sh conflict

--- restart.sh
+++ restart.sh
@@ -10,8 +10,9 @@ else
   # Log this occurange
   echo "$(date) Darm, UberNews crashed again! :(" >> ./crash-log.txt
   # Restart the Sinatra app
-  ruby app.rb > ./app-output.txt &
+  nohup /usr/local/rvm/rubies/ruby-1.8.7-p371/bin/ruby app.rb > ./app-output.txt &
   # Restart Apache because for some reason it no longer forwards correctly to
   # the application port once it restarts---TODO investigate
   /etc/init.d/apache2 restart
 fi
+
